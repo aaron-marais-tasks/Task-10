fd1f8cdc9f458346aa7f920efea030ba1cb38d87
Aaron Lee Marais <its_me@aaronleem.co.za>
2019-08-06 20:04:43 +0200

    Tabs to spaces

--- src/Components/Block.jsx
+++ src/Components/Block.jsx
@@ -121,7 +121,7 @@ export default class extends React.Component {
     // Flag current block
     flagBlock(e) {
         // Stop context menu from popping up
-    	e.preventDefault()
+        e.preventDefault()
 
         // Stop execution if revealed already
         if(this.state.revealed) return
@@ -130,7 +130,7 @@ export default class extends React.Component {
         if(!this.state.flagged && this.props.maxFlagsReached()) return
 
         // Reverse our flagged state
-    	this.setState({flagged: !this.state.flagged}, () => {
+        this.setState({flagged: !this.state.flagged}, () => {
             // When state is complete, increment or decrement the board flag counter,
             // based on if this was a flag or not
             if(this.state.flagged) this.props.addFlag()
@@ -142,8 +142,8 @@ export default class extends React.Component {
     render() {
         // Optional props; if revealed, set our data-count to the amount
         // of mines near us
-    	const optionalProps = {}
-    	if(this.state.revealed) optionalProps["data-count"] = this.minesNear()
+        const optionalProps = {}
+        if(this.state.revealed) optionalProps["data-count"] = this.minesNear()
 
         return (
             /* Div container for block
@@ -151,50 +151,50 @@ export default class extends React.Component {
                 Pass in optional props
                 On click handler
                 On right click handler */
-        	<div
-        		className={"block" + (
-	            	this.state.revealed ? (" " + (this.state.isMine ? "mine" : "clear")) : ""
-	        	)}
-	        	{...optionalProps}
-	        	onClick={this.revealBlock.bind(this)}
-	        	onContextMenu={this.flagBlock.bind(this)}
-	    	>
+            <div
+                className={"block" + (
+                    this.state.revealed ? (" " + (this.state.isMine ? "mine" : "clear")) : ""
+                )}
+                {...optionalProps}
+                onClick={this.revealBlock.bind(this)}
+                onContextMenu={this.flagBlock.bind(this)}
+            >
                 {/* If it is revealed, show icon or text */}
-	    		{this.state.revealed ? (
+                {this.state.revealed ? (
                     // If is mine, show bomb
-	    			this.state.isMine ? (
-	    				<svg className="bomb" viewBox="0 0 512 512">
-	    					<path fill="currentColor" d={"M384.5 144.5l56-56-17-17-56 56-52.2-52.2c-6.2-6.2-16.4-6.2-22.6 0l-28.4 "+
-	    						"28.4c-17.9-5-36.8-7.7-56.3-7.7C93.1 96 0 189.1 0 304s93.1 208 208 208 208-93.1 "+
-	    						"208-208c0-19.5-2.7-38.4-7.7-56.3l28.4-28.4c6.2-6.2 6.2-16.4 0-22.6l-52.2-52.2zm-30 89.1c7.9 28.2 "+
-	    						"13.5 43.9 13.5 70.4 0 88.4-71.6 160-160 160S48 392.4 48 304s71.6-160 160-160c26.3 0 41.4 5.4 70.4 "+
-	    						"13.5l25.6-25.6 76.1 76.1-25.6 25.6zM512 72c0 6.6-5.4 12-12 12h-24c-6.6 0-12-5.4-12-12s5.4-12 "+
-	    						"12-12h24c6.6 0 12 5.4 12 12zm-60-60v24c0 6.6-5.4 12-12 12s-12-5.4-12-12V12c0-6.6 5.4-12 12-12s12 "+
-	    						"5.4 12 12zm5 43c-4.7-4.7-4.7-12.3 0-17l17-17c4.7-4.7 12.3-4.7 17 0 4.7 4.7 4.7 12.3 0 17l-17 "+
-	    						"17c-4.7 4.7-12.3 4.7-17 0zm-67.9-16.9c-4.7-4.7-4.7-12.3 0-17 4.7-4.7 12.3-4.7 17 0l17 17c4.7 4.7 "+
-	    						"4.7 12.3 0 17-4.7 4.7-12.3 4.7-17 0l-17-17zm101.8 67.8c4.7 4.7 4.7 12.3 0 17-4.7 4.7-12.3 4.7-17 "+
-	    						"0l-17-17c-4.7-4.7-4.7-12.3 0-17 4.7-4.7 12.3-4.7 17 0l17 17zM216 208c0 13.3-10.7 24-24 24-30.9 "+
-	    						"0-56 25.1-56 56 0 13.3-10.7 24-24 24s-24-10.7-24-24c0-57.3 46.7-104 104-104 13.3 0 24 10.7 24 24z"}
-    						/>
-						</svg>
-    				) : 
+                    this.state.isMine ? (
+                        <svg className="bomb" viewBox="0 0 512 512">
+                            <path fill="currentColor" d={"M384.5 144.5l56-56-17-17-56 56-52.2-52.2c-6.2-6.2-16.4-6.2-22.6 0l-28.4 "+
+                                "28.4c-17.9-5-36.8-7.7-56.3-7.7C93.1 96 0 189.1 0 304s93.1 208 208 208 208-93.1 "+
+                                "208-208c0-19.5-2.7-38.4-7.7-56.3l28.4-28.4c6.2-6.2 6.2-16.4 0-22.6l-52.2-52.2zm-30 89.1c7.9 28.2 "+
+                                "13.5 43.9 13.5 70.4 0 88.4-71.6 160-160 160S48 392.4 48 304s71.6-160 160-160c26.3 0 41.4 5.4 70.4 "+
+                                "13.5l25.6-25.6 76.1 76.1-25.6 25.6zM512 72c0 6.6-5.4 12-12 12h-24c-6.6 0-12-5.4-12-12s5.4-12 "+
+                                "12-12h24c6.6 0 12 5.4 12 12zm-60-60v24c0 6.6-5.4 12-12 12s-12-5.4-12-12V12c0-6.6 5.4-12 12-12s12 "+
+                                "5.4 12 12zm5 43c-4.7-4.7-4.7-12.3 0-17l17-17c4.7-4.7 12.3-4.7 17 0 4.7 4.7 4.7 12.3 0 17l-17 "+
+                                "17c-4.7 4.7-12.3 4.7-17 0zm-67.9-16.9c-4.7-4.7-4.7-12.3 0-17 4.7-4.7 12.3-4.7 17 0l17 17c4.7 4.7 "+
+                                "4.7 12.3 0 17-4.7 4.7-12.3 4.7-17 0l-17-17zm101.8 67.8c4.7 4.7 4.7 12.3 0 17-4.7 4.7-12.3 4.7-17 "+
+                                "0l-17-17c-4.7-4.7-4.7-12.3 0-17 4.7-4.7 12.3-4.7 17 0l17 17zM216 208c0 13.3-10.7 24-24 24-30.9 "+
+                                "0-56 25.1-56 56 0 13.3-10.7 24-24 24s-24-10.7-24-24c0-57.3 46.7-104 104-104 13.3 0 24 10.7 24 24z"}
+                            />
+                        </svg>
+                    ) : 
                     // If not bomb, show text (amount of mines close)
                     this.minesNear()
-    			) : (
+                ) : (
                     // If flagged, show flag icon
-    				this.state.flagged ? (
-		    			<svg className="flag" viewBox="0 0 512 512">
-		    				<path fill="currentColor" d={
-		    					"M349.565 98.783C295.978 98.783 251.721 64 184.348 64c-24.955 0-47.309 4.384-68.045 12.013a55.947 55.947 "+
-		    					"0 0 0 3.586-23.562C118.117 24.015 94.806 1.206 66.338.048 34.345-1.254 8 24.296 8 56c0 19.026 9.497 35.825 "+
-		    					"24 45.945V488c0 13.255 10.745 24 24 24h16c13.255 0 24-10.745 24-24v-94.4c28.311-12.064 63.582-22.122 "+
-		    					"114.435-22.122 53.588 0 97.844 34.783 165.217 34.783 48.169 0 86.667-16.294 122.505-40.858C506.84 359.452 "+
-		    					"512 349.571 512 339.045v-243.1c0-23.393-24.269-38.87-45.485-29.016-34.338 15.948-76.454 31.854-116.95 31.854z"
-	    					} />
-						</svg>
-	    			) : null
-    			)}
-    		</div>
-		)
+                    this.state.flagged ? (
+                        <svg className="flag" viewBox="0 0 512 512">
+                            <path fill="currentColor" d={
+                                "M349.565 98.783C295.978 98.783 251.721 64 184.348 64c-24.955 0-47.309 4.384-68.045 12.013a55.947 55.947 "+
+                                "0 0 0 3.586-23.562C118.117 24.015 94.806 1.206 66.338.048 34.345-1.254 8 24.296 8 56c0 19.026 9.497 35.825 "+
+                                "24 45.945V488c0 13.255 10.745 24 24 24h16c13.255 0 24-10.745 24-24v-94.4c28.311-12.064 63.582-22.122 "+
+                                "114.435-22.122 53.588 0 97.844 34.783 165.217 34.783 48.169 0 86.667-16.294 122.505-40.858C506.84 359.452 "+
+                                "512 349.571 512 339.045v-243.1c0-23.393-24.269-38.87-45.485-29.016-34.338 15.948-76.454 31.854-116.95 31.854z"
+                            } />
+                        </svg>
+                    ) : null
+                )}
+            </div>
+        )
     }
 }
